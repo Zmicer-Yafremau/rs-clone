4b9739d3817c9d5223edb7b14018cf0ebe1b3722
Zmicer-Yafremau <dmitry.mickevich@gmail.com>
2023-02-05 14:18:42 +0300

    feat: add pass hiding and links

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import './global.scss';
 import './styles/main.scss';
-import './styles/login.scss';
+import './styles/authorization.scss';
 import '../node_modules/normalize.css/normalize.css';
 import 'bootstrap';
 import './app';
