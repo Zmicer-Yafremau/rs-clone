d37b34b7a03bc386c2f1846215dcf3eb19a9b84f
Zmicer-Yafremau <dmitry.mickevich@gmail.com>
2023-02-05 21:44:15 +0300

    feat: add fetch to register and db for faq

--- src/controller/check-validation.ts
+++ src/controller/check-validation.ts
@@ -1,10 +1,24 @@
+import { Authorization } from '../model/authorization';
 export function checkValidation(form: HTMLFormElement) {
     form.addEventListener(
         'submit',
-        (event) => {
+        async (event) => {
             if (!form.checkValidity()) {
                 event.preventDefault();
                 event.stopPropagation();
+            } else {
+                event.preventDefault();
+                event.stopPropagation();
+                const USR = new Authorization();
+                const FORM_TYPE = form.classList[1].split('_')[0];
+                const INPUTS = (document.getElementsByClassName(
+                    `${FORM_TYPE}__input`
+                ) as unknown) as NodeListOf<HTMLInputElement>;
+                const NAME = INPUTS[0].value;
+                const PHONE = INPUTS[1].value;
+                const MAIL = INPUTS[2].value;
+                const PASS = INPUTS[3].value;
+                await USR.create(NAME, PHONE, MAIL, PASS);
             }
             form.classList.add('was-validated');
         },
