22ac6561e6030b0330ef5b0254f2e57d70cf865f
Kristina_Klepik <kristygomel@gmail.com>
2023-02-07 16:39:03 +0300

    feat: add classes for fetching box, card, feedback

---
+++ src/model/card.ts
@@ -0,0 +1,57 @@
+import { ICard } from '../types/requestTypes';
+
+export class Card {
+    url: string;
+    constructor() {
+        this.url = `https://santa-secret-clone.up.railway.app/card`;
+    }
+    async getCardsOfBox(id: number): Promise<ICard[]> {
+        return (await fetch(`${this.url}?id=${id}`)).json();
+    }
+    async update(
+        id: number,
+        userName: string,
+        wardId: number,
+        cardImg: string,
+        randomKey: string,
+        wishes: string,
+        boxId: number
+    ): Promise<ICard> {
+        return (
+            await fetch(`${this.url}`, {
+                method: 'PUT',
+                body: JSON.stringify({ id, userName, wardId, cardImg, randomKey, wishes, boxId }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+        ).json();
+    }
+    async create(
+        userName: string,
+        wardId: number,
+        cardImg: string,
+        randomKey: string,
+        wishes: string,
+        boxId: number
+    ): Promise<ICard> {
+        const response = await fetch(`${this.url}`, {
+            method: 'POST',
+            body: JSON.stringify({ userName, wardId, cardImg, randomKey, wishes, boxId }),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+            },
+        });
+        const result = await response.json();
+        return result;
+    }
+
+    async delete(id: number) {
+        return (
+            await fetch(`${this.url}/${id}`, {
+                method: 'DELETE',
+            })
+        ).json();
+    }
+}
