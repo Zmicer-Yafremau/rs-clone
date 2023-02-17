dbe69b92f896e1c9da6691cb6e833727088742fc
Kristina_Klepik <kristygomel@gmail.com>
2023-02-17 20:52:27 +0300

    feat: add error page and delete console.logs

--- src/view/errorView/index.ts
+++ src/view/errorView/index.ts
@@ -1,10 +1,24 @@
 import { Model } from '../../model/index';
 import { Controller } from '../../controller';
+import { errorCats } from '../../db/errorCats';
 
 export class ErrorView {
     constructor(private controller: Controller, private model: Model, private root: Element) {}
 
     render() {
-        this.root.innerHTML = `<div>This page not found</div>`;
+        this.root.innerHTML = `<div class="box__not" >${errorCats.boxNotFound}
+        <div><p>Упс....</p><p>Похоже, вы перешли по неверной ссылке, возвращайтесь домой...</p></div>
+        <button id="back" type="button" class="btn main__button bg-light active">
+                        Вернуться на главную
+                    </button></div>`;
+        this.addListeners();
+    }
+    addListeners() {
+        const buttonBack = document.querySelector('#back');
+        if (buttonBack) {
+            buttonBack.addEventListener('click', (e) => {
+                this.controller.route(location.origin + '/');
+            });
+        }
     }
 }
