f1b95bb3f1b0c8f33616f3dc7d6a5b31a36f37bf
Zmicer-Yafremau <dmitry.mickevich@gmail.com>
2023-02-22 19:33:37 +0300

    feat: add card check

--- src/controller/delete-user.ts
+++ src/controller/delete-user.ts
@@ -9,12 +9,18 @@ export async function deleteUser() {
     const U_BOX = new UserBoxes();
     const U_BOX_OBJ = await U_BOX.getByUserId(localStorage.id);
     U_BOX_OBJ[0].user_boxes.forEach(async (box_id) => {
-        const CARDS = await CARD.getCardsOfBox(box_id);
-        CARDS.forEach((box_card) => {
-            if ((box_card.user_id = localStorage.id)) {
-                CARD.delete(box_card.card_id);
-            }
-        });
+        const NOT_ADMIN_BOX = await BOX.getByBoxId(box_id);
+        if (NOT_ADMIN_BOX.is_draw) {
+            console.log('cant');
+            return;
+        } else {
+            const CARDS = await CARD.getCardsOfBox(box_id);
+            CARDS.forEach((box_card) => {
+                if ((box_card.user_id === localStorage.id)) {
+                    CARD.delete(box_card.card_id);
+                }
+            });   
+        }
     });
     const ADMIN_BOX_ARR = await BOX.getByAdminId(localStorage.id);
     ADMIN_BOX_ARR.forEach(async (box_obj) => {
