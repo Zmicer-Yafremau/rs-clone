b6f415ad0a51841d99691957e90f9cef5ec21e2a
Zmicer-Yafremau <dmitry.mickevich@gmail.com>
2023-02-26 21:30:28 +0300

    feat: add notes

---
+++ src/view/mainView/create-note.ts
@@ -0,0 +1,14 @@
+export function createNote(note: string) {
+    const NOTE = document.createElement('div');
+    NOTE.classList.add('note');
+    NOTE.classList.add('center');
+    NOTE.innerHTML = `<div class="toast-body between">
+${note}
+<button type="button" class="btn-close note__close mx-3" data-bs-dismiss="toast" aria-label="Close"></button>
+</div>`;
+
+    const NOTIFICATIONS_CONTENT = document.getElementsByClassName(`notifications__content`)[0] as HTMLDivElement;
+    if (NOTIFICATIONS_CONTENT) {
+        NOTIFICATIONS_CONTENT.appendChild(NOTE);
+    }
+}
